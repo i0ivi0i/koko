9d7b6f28408b4c8f3333137b07d1202c5b802c25
i0ivi0i <tiktokincn@gmail.com>
2026-04-04 16:53:03 +0800

    将 run.ps1 收敛为极简开发启动器

--- run.ps1
+++ run.ps1
@@ -1,13 +1,3 @@
-$ErrorActionPreference = "Stop"
-$repoRoot = $PSScriptRoot
-$xtaskArgs = @("xtask", "dev") + $args
-
-# PowerShell 这里只是 Windows 开发入口薄壳；开发编排真相统一交给 cargo xtask dev。
-Push-Location $repoRoot
-try {
-    & cargo @xtaskArgs
-    exit $LASTEXITCODE
-}
-finally {
-    Pop-Location
-}
+Set-Location $PSScriptRoot
+& cargo xtask dev @args
+exit $LASTEXITCODE
